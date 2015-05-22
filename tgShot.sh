247e71e7315f3857395b6e6a2cbed984191602f8
kolbdaniel1 <kolbdaniel1@gmail.com>
2015-05-22 12:27:17 +0000

    add optional string to be sent before the photo

--- tgShot.sh
+++ tgShot.sh
@@ -5,10 +5,18 @@ fswebcam -r 1920x1080  --no-banner /home/pi/photos/$DATE.jpg
 
 now=$(date)
 to=$1
+shift
+optionalText=$@
 tgpath=/home/pi/tg
 LOGFILE="/var/log/tg.log"
 cd ${tgpath}
-(sleep 5; echo "send_photo $to /home/pi/photos/$DATE.jpg"; echo "safe_quit") | /home/pi/tg/bin/telegram-cli -W
+(sleep 5; echo "send_photo $to /home/pi/photos/$DATE.jpg"; echo "msg $to $optionalText";echo "safe_quit") | $tgpath/bin/telegram-cli -W
 echo "$now = $to Recipient Photo  = $DATE.jpg" >> ${LOGFILE}
-echo "Finished" >> ${LOGFILE}
+echo "Finished by user:" >> ${LOGFILE}
+who am i | awk '{print $1}' >> ${LOGFILE}
+rm /home/pi/photos/$DATE.jpg
+
+
+
+
 
